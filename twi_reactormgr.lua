528e1c5f828cc8ae3739e27daf6f6daaae5e05ce
Twill <twill@arcenvoy.com>
2019-03-01 10:24:08 +1100

    Add Box-drawing functionality

--- twi_reactormgr.lua
+++ twi_reactormgr.lua
@@ -32,6 +32,35 @@ function comma_value(amount)
   return formatted
 end
 
+function draw_box(pos_x, pos_y, width, height, title, color, background)
+	gpu.setForeground(color)
+	gpu.setBackground(background)
+	local title_len = string.len(title)
+	if math.fmod(title_len,2) == 1 then
+		title = title .. "═"
+		title_len = title_len + 1
+	end
+	if math.fmod(width, 2) == 0 then
+		local bar_adjust = (width - (2 + title_len))/2
+		local workingstr = "╔" .. string.rep("═",bar_adjust) .. title .. string.rep("═",bar_adjust) .. "╗"
+		gpu.set(pos_x, pos_y, workingstr)
+	elseif math.fmod(width, 2) == 1 then
+		local bar_adjust = math.ceil((width - (2 + title_len))/2)
+		local workingstr = "╔" .. string.rep("═",bar_adjust) .. title .. string.rep("═",bar_adjust - 1) .. "╗"
+		gpu.set(pos_x, pos_y, workingstr)
+	end
+	--local workingstr = "╔" .. string.rep("═",(width - 2)) .. "╗"
+	--gpu.set(pos_x, pos_y, workingstr)
+	gpu.set(pos_x, pos_y + 1, string.rep("║",(height - 2)), true)
+	gpu.set(pos_x + width - 1, pos_y + 1, string.rep("║",(height - 2)), true)
+	workingstr = "╚" .. string.rep("═",(width - 2)) .. "╝"
+	gpu.set(pos_x, pos_y + height - 1, workingstr)
+end
+
+-------------------
+---- Main Loop  ---
+-------------------
+
 while true do
 	local cell_level = cell.getEnergy() -- no convertion yet to save cycles?
 	if cell_level > (cell.getMaxEnergy() * cell_cap_pct) then
@@ -47,5 +76,6 @@ while true do
 	last_cell_level = cell_level -- still not converting because we really only need to for display I guess?
 	
 	io.write("cell level " .. comma_value(tostring(cell_level * J_to_RF_factor)) .. " RF \n")
+	draw_box(1, 1, 20, 20, "abbcce", 0xffffff, 0x000000)
 	os.sleep(2)
 end
